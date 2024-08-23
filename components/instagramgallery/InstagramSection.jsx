3b95844fadce3800723175a36c649f319dfb1647
Favfly Dev <favfly.dev@gmail.com>
2024-08-23 19:16:15 +0530

    update

--- components/instagramgallery/InstagramSection.jsx
+++ components/instagramgallery/InstagramSection.jsx
@@ -3,9 +3,13 @@ const InstagramSection = ({ slice }) => {
     <>
       <div
         loading="lazy"
-        data-mc-src="f8236406-68ae-4abc-8a85-06206dca4b4d#instagram"
+        data-mc-src="bcdf48aa-de2a-4051-8bce-bb4e68be777b#instagram"
       ></div>
-      <script src={slice?.primary?.woxo_id} async data-usrc></script>
+      <script
+        src="https://cdn2.woxo.tech/a.js#65424435be0658d5e4531c3a"
+        async
+        data-usrc
+      ></script>
     </>
   );
 };
