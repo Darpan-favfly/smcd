77c2cd5669d8a0ca70b8aade46b08f47bd2cd1c2
Favfly Dev <favfly.dev@gmail.com>
2024-08-24 12:55:20 +0530

    update

--- components/category/CategoriesSection.jsx
+++ components/category/CategoriesSection.jsx
@@ -1,8 +1,42 @@
+"use client";
 import { PrismicNextImage, PrismicNextLink } from "@prismicio/next";
 import HeadingComponent from "../heading/HeadingComponent";
 
+import { useEffect, useRef } from "react";
+
 const CategoriesSection = ({ slice }) => {
   const { heading, sub_heading, video, items, link } = slice?.primary;
+
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    const videoElement = videoRef.current;
+
+    const onIntersection = (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          const sourceElement = videoElement.querySelector("source");
+          sourceElement.src = sourceElement.dataset.src;
+          videoElement.load();
+        }
+      });
+    };
+
+    const observer = new IntersectionObserver(onIntersection, {
+      rootMargin: "0px",
+      threshold: 0.1,
+    });
+
+    if (videoElement) {
+      observer.observe(videoElement);
+    }
+
+    return () => {
+      if (videoElement) {
+        observer.unobserve(videoElement);
+      }
+    };
+  }, []);
 
   return (
     <>
@@ -31,8 +65,14 @@ const CategoriesSection = ({ slice }) => {
           {/* // ===== 2ND COLUMN ===== */}
           <div className="col-sm-4 video-container category-col order-0">
             <PrismicNextLink field={link} className="overflow-hidden h-100">
-              <video className="full-height-video" autoPlay muted loop>
-                <source src={video.url} type="video/mp4" />
+              <video
+                ref={videoRef}
+                className="full-height-video"
+                autoPlay
+                muted
+                loop
+              >
+                <source data-src={video.url} type="video/mp4" />
               </video>
             </PrismicNextLink>
           </div>
